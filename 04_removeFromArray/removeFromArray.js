97cc6a65d28a5b26afedcf8322e785fb12ea7e39
Henry Beveridge <henrydbeveridge@gmail.com>
2022-06-09 08:32:06 -0700

    Solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+    return array.filter(element => {
+        let shouldAddItem = true;
+        args.forEach(arg => {
+            if (element === arg) {
+                shouldAddItem = false;
+            }
+        })
+        return shouldAddItem;
+    });
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 1, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
