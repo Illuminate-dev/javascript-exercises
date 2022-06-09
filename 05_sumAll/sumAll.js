e4438a129d05e519fc2510bb87075b0bada3f7dd
Henry Beveridge <henrydbeveridge@gmail.com>
2022-06-09 08:41:31 -0700

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
 
+    if (start < 0 || end < 0) {
+        return 'ERROR';
+    }
+
+    if (typeof (start) !== "number" || typeof (end) !== "number") {
+        return 'ERROR';
+    }
+
+
+    if (start > end) {
+        tempEnd = end;
+        end = start;
+        start = tempEnd;
+    }
+
+
+    let sum = 0;
+    for (i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
